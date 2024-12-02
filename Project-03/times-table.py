fb59a8b373b136a290eba8e3bc5c37e4f4d8f125
illia-amini <illiaamini2002ir@gmail.com>
2024-12-02 05:15:36 +0330

    project-03 updated

---
+++ Project-03/times-table.py
@@ -0,0 +1,15 @@
+"""
+table1=[1,2,3,4,5,6,7,8,9,10]
+table2=[1,2,3,4,5,6,7,8,9,10]
+i=0
+while i<len(table1):
+    print(table1[i],end=' ')
+    j=0
+    while j<table2[i]:
+        print(time,'=',table1[i]*table2[j],end='')
+        time=1
+        while time<
+        j+=1
+    i+=1
+    print('')
+"""
