1690963341c9095900649a41ad313c18b421705e
illia-amini <illiaamini2002ir@gmail.com>
2024-12-03 05:27:59 +0330

    Update students info.py

--- Project-04/students info.py
+++ Project-04/students info.py
@@ -6,17 +6,21 @@ keep reading...
 if name=0
 finish
 '''
-list=[]
-while 1:
-    name=input('name=')
-    if name=='0':
-        break
-    age=int(input('age='))
-    city=input('city=')
-    dict={}
-    dict['name:']=age
-    dict['age:']=age
-    dict['city:']=city
-    list.append(dict)
-for item in list:#each item in list is dict originally
-    print(item)
+try:
+    list=[]
+    while 1:
+        name=input('name=')
+        if name=='0':
+         break
+        age=int(input('age='))
+        city=input('city=')
+        dict={}
+        dict['name:']=age
+        dict['age:']=age
+        dict['city:']=city
+        list.append(dict)
+        for item in list:#each item in list is dict originally
+            print(item)
+except:
+    print("Error input is not number in age")
+print('end')
