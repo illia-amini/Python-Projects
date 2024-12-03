4b814f33bef1168c024d6f14fdbfb57adb6a2dde
illia-amini <illiaamini2002ir@gmail.com>
2024-12-03 04:01:36 +0330

    project-03 updated
    add or del name, 3th of december

---
+++ Project-03/add or del student name.py
@@ -0,0 +1,30 @@
+'''
+choose;1-add or 2-remove
+1
+you want to add:)
+Enter ur name;
+ali
+['ali']
+choose;1-add or 2-remove
+2
+you want to remove:(
+Enter ur name;
+ali
+['']
+3
+Are you ...!!!!,choose between either 1 or 2 broo
+'''
+studentnames=[]
+while 1:# while 1 means while true,moreover,while 0 means while false
+    select=int(input('Choose 1.add or 2.delete\n'))#\n,will creat a new line
+    if select==1:
+        print("you want to add:)")
+        name=input('Enter ur name:')
+        studentnames.append(name)
+        print(studentnames)
+    elif select==2:
+        print('you want to remove:(')
+        name=input('Enter ur name:')
+        studentnames.remove(name)
+        print(studentnames)
+    #mind-of-king-,-hear-of-warrior
