af969709c370bcccd6aeff1855b8f170aa5b0841
illia-amini <illiaamini2002ir@gmail.com>
2024-12-10 20:57:19 +0330

    Update pr-02.py

--- Py-book-Pr/pr-02.py
+++ Py-book-Pr/pr-02.py
@@ -1,2 +1,15 @@
 #ex-02
 #Operations
+#01-Mathematic Operations;
+# +,/,-,*,//,**
+#ex
+num1 = int(input('enter num1;'))
+num2 = int(input('Enter ur num2;'))
+print('sum;',num1 + num2)
+print('mince;',num1 - num2)
+print('divison;',num1 / num2)
+print('time;',num1 * num2)
+print('power;',num1 ** num2)
+print('divioin(int);',num1 // num2)
+#02-Comprasion Operation;
+# ==,!=,<,>,>=,<=
