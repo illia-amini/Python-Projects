b00d73b0e3c3d9bf6fafdaaef705e314d621a3f9
illia-amini <illiaamini2002ir@gmail.com>
2024-12-11 00:28:17 +0330

    Update pr-02.py

--- Py-book-Pr/pr-02.py
+++ Py-book-Pr/pr-02.py
@@ -1,7 +1,7 @@
 #ex-02
 #Operations
 #01-Mathematic Operations;
-# +,/,-,*,//,**
+# +,/,-,*,//,**,%
 #ex
 num1 = int(input('enter num1;'))
 num2 = int(input('Enter ur num2;'))
@@ -11,16 +11,23 @@ print('divison;',num1 / num2)
 print('time;',num1 * num2)
 print('power;',num1 ** num2)
 print('divioin(int);',num1 // num2)
+print('%;',num1%num2)
+#ex02
+num3 = int(input('enter num1;'))
+num4 = int(input('Enter ur num2;'))
+num3+=num4#also you can write -=,/=,%=,//=
+print(num3)
 #02-Comprasion Operation;
 # ==,!=,<,>,>=,<=
-num3 = int(input('enter ur num1;'))
-num4 = int(input('enter ur num2;'))
-if num3 == num4:
+#ex
+num5 = int(input('enter ur num1;'))
+num6 = int(input('enter ur num2;'))
+if num5 == num6:
     print('==')
-elif num3 != num4:
+elif num5 != num6:
     print('!=')
-    if num3 < num4:
+    if num5 < num6:
         print('<')
-    elif num3 > num4:
+    elif num5 > num6:
         print('>') 
 
