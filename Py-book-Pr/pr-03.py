ef9b5feaf07b5ead80edf0dad6af4f4d0beab047
illia-amini <illiaamini2002ir@gmail.com>
2024-12-11 00:48:37 +0330

    Create pr-03.py

---
+++ Py-book-Pr/pr-03.py
@@ -0,0 +1,11 @@
+#wirte a code to returns the area of rectangle
+lenght = int(input('Enter ur length;'))
+width = int(input('Enter ur width;'))
+print('Area;',lenght*width)
+#write a code to convert meter to km
+lenght2 = int(input('Entre ur length;'))
+print('km;',lenght2/1000)  
+#write a code for date of birth
+date_of_birth = int(input('Enter ur date of birth;'))
+result =f'ur age on 2100 would be;{2100 - date_of_birth},\nif u dont die sooner hhhh!!!!\njust a joke' 
+print(result)
