143f2f0950f611b8e39ba8704d228b45cc823b04
illia-amini <illiaamini2002ir@gmail.com>
2024-12-12 04:38:15 +0330

    projects updatead

---
+++ Py-book-Pr/pr-07.py
@@ -0,0 +1,6 @@
+#write a code,return if the sqrt of user number excist or a normal number
+Number1=int(input('Enter ur number;'))
+if Number1**0.5==int(Number1**0.5):
+    print('yes')
+else:
+    print("no")
