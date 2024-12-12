cceb8eff42e05432838f7ceff7c4f95fdaae4148
illia-amini <illiaamini2002ir@gmail.com>
2024-12-12 04:20:13 +0330

    Update pr-05.py

--- Py-book-Pr/pr-05.py
+++ Py-book-Pr/pr-05.py
@@ -12,6 +12,12 @@ if Number4<Minn:
     print('the fourth number is min:',Number4)
 else:
     print('the first number is min:',Number1)
+#================= Another-methode==================
+Number1 = int(input("Enter ur first number:"))
+Number2 = int(input("Enter ur second number:"))
+Number3 = int(input("Enter ur third number:"))
+Number4 = int(input("Enter ur fourth number:"))
+print('ur min num is;',min(Number1,Number2,Number3,Number4))
 #write a code,returns the max among 4 user number.
 User_Number1 = int(input("Enter ur first number:"))
 User_Number2 = int(input("Enter ur second number:"))
@@ -26,3 +32,9 @@ if User_Number4>Maxx:
     print('the fourth number is max:',User_Number4)
 else:
     print('the first number is max:',User_Number1)
+#================= Another-methode==================
+User_Number1 = int(input("Enter ur first number:"))
+User_Number2 = int(input("Enter ur second number:"))
+User_Number3 = int(input("Enter ur third number:"))
+User_Number4 = int(input("Enter ur fourth number:"))
+print('ur max num is;',max(User_Number1,User_Number2,User_Number3,User_Number4))
