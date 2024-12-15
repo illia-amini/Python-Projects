043cbce66dc0e020919d645cec55ff04d4b667a4
illia-amini <illiaamini2002ir@gmail.com>
2024-12-15 23:35:50 +0330

    Update pr-10.py

--- Py-book-Pr/pr-10.py
+++ Py-book-Pr/pr-10.py
@@ -4,7 +4,7 @@ pass_word = 'illia_amini2002'
 print('welcome to our website;'),print('if u already have a account,enter 1'),print('if u dont have a account yet,enter 2')
 enteread_num_01 = int(input("here:"))
 if enteread_num_01 == 1:
-    print('for ur securtiy, chose one puzzle below by its number'),print('1-Math operatin'),print('2-Geometri operation;'),print('3_Min or Max'),print('4_Second to hour or minute'),print('5-Capitall or small')
+    print('for ur securtiy, chose one puzzle below by its number'),print('1-Math operatin'),print('2-Geometri operation;'),print('3_Min or Max'),print('4_Second to hour or minute'),print('5-Word check')
     enteread_num_01_02 =int(input('here;'))
     if enteread_num_01_02 == 1:
         user_num_01 =int(input('Enter ur  first number;'))
@@ -81,9 +81,12 @@ if enteread_num_01 == 1:
     elif enteread_num_01_02 == 4:
         pass
     elif enteread_num_01_02 == 5:
-        pass
-    elif enteread_num_01_02 == 5:
-        pass
+        user_text = input('Enter ur text:')
+        print('word A will be checked.')
+        if 'a' in user_text and 'A' in user_text:
+            print('Yes')
+        else:
+            print('No')
     else:
         print('Enter a valid number!!!')
 if enteread_num_01 == 2:
