91948d8fbbb7094a57bd7211f2ca1e3bd29aa40b
illia-amini <illiaamini2002ir@gmail.com>
2024-12-30 14:23:34 +0330

    Update Pyramid.py

--- Projects-07/Pyramid.py
+++ Projects-07/Pyramid.py
@@ -1,8 +1,8 @@
 user_num=int(input('Enter ur num:'))
 count=0
-for i in range(1,user_num+1):
-    for j in  range (1,(user_num-i)+1):
-        print(end='  ')
+for i in range(1,user_num+1):#for *
+    for j in  range (1,(user_num-i)+1):#for sapces
+        print(end='  ')#make 2 spaces here
     while count!=(2*i-1):
         print("* ", end="")
         count += 1
